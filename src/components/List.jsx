b6d369da3f7b613fb30eccf6420a4569482ca11c
beellz <belal20795@gmail.com>
2020-05-24 22:25:56 +0530

    made using the components method

---
+++ src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function List() {
+    return (
+    <ul>
+        <li>chicken</li>
+        <li>mutton gravy</li>
+        <li>wonton chicken soup</li>
+    </ul>
+    );
+}
+
+export default List; 
